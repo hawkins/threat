14bf7241426f5a3ee8fa7c7c58d0abe9ffdf0088
Josh Hawkins <hawkins@users.noreply.github.com>
2019-01-10 20:57:12 -0500

    Add configure, test executing the request

--- lib/threat.rb
+++ lib/threat.rb
@@ -5,21 +5,6 @@ require 'threat/version'
 require 'threat/readonlyqueue'
 
 module Threat
-  @@inbox = Queue.new
-  @@store = {}
-  @@outbox = Queue.new
-
-  @@scheduler = Thread.new do
-    loop do
-      # TODO: How can we more effectively not waste too many cycles?
-      sleep(0.1)
-
-      unless @@inbox.empty?
-        # TODO: Spawn agents to process the inbox
-      end
-    end
-  end
-
   def self.inbox
     ReadOnlyQueue.new @@inbox
   end
@@ -40,19 +25,56 @@ module Threat
     end
   end
 
+  ##
+  # Configure the underlying HTTP client
+  #
+  # Note this also resets the library, discarding all requests, agents ...
+  #
+  # Accepts a block, used to expose the HTTPClient instance:
+  #
+  #   Thread::configure do |httpclient|
+  #     # do whatever you want with the httpclient, for instance:
+  #     httpclient.cookie_manager = nul
+  #   end
+  def self.configure
+    yield CLIENT if block_given?
+
+    @@inbox = Queue.new
+    @@store = {}
+    @@outbox = Queue.new
+    @@threads = []
+  end
+
   private
 
   CLIENT = HTTPClient.new
-  CLIENT.cookie_manager = nil
+
+  @@scheduler = Thread.new do
+    loop do
+      # TODO: How can we more effectively not waste too many cycles?
+      sleep(0.1)
+
+      next if @@inbox.empty?
+
+      # TODO: Check rate-limiting
+
+      # Spawn an agent if safe to do so
+      @@threads << Thread.new do
+        a = Agent.new
+        request = @@inbox.pop
+        a.invoke request, @@outbox
+      end
+    end
+  end
 
   class Agent
     def initialize
       @client = CLIENT.clone
     end
 
-    def invoke(request)
+    def invoke(request, outbox)
       res = @client.send(request[:method], request[:uri], request[:args])
-      @@outbox.push(id: request[:id], res: res)
+      outbox.push(id: request[:id], res: res)
       res
     end
   end
--- spec/threat_spec.rb
+++ spec/threat_spec.rb
@@ -1,4 +1,8 @@
 RSpec.describe Threat do
+  before(:each) do
+    Threat::configure
+  end
+
   it "has a version number" do
     expect(Threat::VERSION).not_to be nil
   end
@@ -6,5 +10,12 @@ RSpec.describe Threat do
   it "can queue requests" do
     Threat::request(:get, "https://github.com/hawkins/threat")
     expect(Threat::inbox.size).to eq(1)
+  end
+
+  it "schedules and executes requests" do
+    Threat::request(:get, "https://github.com/hawkins/threat")
+    sleep(3)
+    expect(Threat::inbox.size).to eq(0)
+    expect(Threat::outbox.size).to eq(1)
   end
 end
